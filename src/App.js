45a64a8927e477aa2a5ea18dd31c87a22d231cd3
JSkrzynski <s23132@pjwstk.edu.pl>
2023-01-22 20:03:01 +0100

    shaqtus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React, {useState, useEffect} from "react";
+
+import Person from "./components/Person";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [person, setPerson] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    async function fetchPerson() {
+        fetch("https://randomuser.me/api/")
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then(person => setPerson(person))
+            .catch(error => {
+                console.log(error);
+                setError(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }
+
+    useEffect(() => {
+        fetchPerson();
+    }, []);
+
+    if (loading) return <h1>loading...</h1>;
+    if (error) return <pre>{JSON.stringify(error)}</pre>;
+
+    return (
+        <>
+            <Person person={person}/>
+            <button onClick={() => fetchPerson()}>Roll new</button>
+        </>
+    );
 }
 
 export default App;
